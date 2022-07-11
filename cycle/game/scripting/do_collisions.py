841734c28e5e58b6982d9923a376be25290fd04b
CalebHatch <calebhatchcontact@gmail.com>
2022-07-11 11:47:46 -0600

    Update do_collisions.py

--- cycle/game/scripting/do_collisions.py
+++ cycle/game/scripting/do_collisions.py
@@ -1,4 +1,5 @@
 import constants
+from cycle.game.casting.player import Player
 
 from game.scripting.action import Action
 from game.shared.coordinate import Coordinate
@@ -13,7 +14,7 @@ class DoCollisions(Action):
         self._is_game_over = False
         self._game_over_message = ""
 
-    def execute(self, cast, script):
+    def execute(self, cast):
 
         if not self._is_game_over:
             self._do_collision(cast)
@@ -22,14 +23,14 @@ class DoCollisions(Action):
     def _do_collision(self):
         pass
 
-    def _do_game_over(self, cast):
+    def _do_game_over(self):
         x = int(constants.MAX_X / 2)
         y = int(constants.MAX_Y / 2)
         position = Coordinate(x, y)
 
         if self._is_game_over:
             game_over = GameOver(position)
-            game_over.set_text(self._game_over_message)
-            game_over.set_font_size(50)
+            game_over.set_text(GameOver(Player))
+            game_over.set_font_size(45)
 
 
