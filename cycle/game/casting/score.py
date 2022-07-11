092fcf2faaf597d32b41b876df4f8d8f6d426554
CalebHatch <calebhatchcontact@gmail.com>
2022-07-11 10:54:33 -0600

    Update score.py

--- cycle/game/casting/score.py
+++ cycle/game/casting/score.py
@@ -12,11 +12,6 @@ class _Score(Player):
         self.set_text(f"{self._player_name}: {self._coordinates}")
         self.set_position(position)
 
-    def add_coordinates(self, coordinates):
-        
-        self._coordinates += coordinates
-        self.set_text(f"{self._player_name}: {self._coordinates}")
-
     def get_score(self):
         
         return self._score
@@ -24,9 +19,4 @@ class _Score(Player):
     def lower_score(self):
        
         self._coordinates -= 1
-        self.set_text(f"{self._player_name}: {self._coordinates}")
-
-    def set_player_name(self,name):
-        
-        self._player_name = name
         self.set_text(f"{self._player_name}: {self._coordinates}")
